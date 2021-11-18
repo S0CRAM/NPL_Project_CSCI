88ca6f521045a342d00f32900ec0d8b48a569d7f
sakura <m.aguiar0954@gmail.com>
2021-11-18 12:13:07 -0500

    v1.0

--- src/Driver.java
+++ src/Driver.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 // Program
 public class Driver {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner twt = new Scanner(new File("./twitter.csv"));
+        Scanner twt = new Scanner(new File("./twitter.csv")); // the ./ allows us to move up a folder
         twt.useDelimiter("\n");
         while (twt.hasNext()) {
             System.out.println(twt.next());
         }
         twt.close();
-    } // the ./ allows us to move up a folder
+    }
 }
